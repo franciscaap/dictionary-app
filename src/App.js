96956edd467fe1f2c82c227925df890b70f6d043
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-06-19 12:20:27 +0100

    Made changes to initial commit of App

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Welcome
-        </a>
-      </header>
+      <header className="App-header"></header>
+      <a
+        className="App-logo text-decoration-none"
+        href="/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        F P
+      </a>
     </div>
   );
 }
