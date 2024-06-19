0b846d4b10f81442e8fa2c79f63b48eb5897a13a
franciscaap <155903837+franciscaap@users.noreply.github.com>
2024-06-19 18:46:23 +0100

    Integrated dictionary API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 export default function Dictionary() {
   const [keyword, setKeyword] = useState(null);
 
+  function searchKeyword(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
-    alert(`Searching for ${keyword}`);
+    event.preventDefault();
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+
+    axios.get(apiUrl).then(searchKeyword);
   }
 
   function updateSearch(event) {
-    event.preventDefault();
     setKeyword(event.target.value);
   }
 
